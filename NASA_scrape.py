2b1f2e31491d7d5adbc08895c1baac4241fdf6a9
Lin Taylor <taylorxlin@gmail.com>
2016-01-05 14:30:29 -0500

    Working scraper

--- NASA_scrape.py
+++ NASA_scrape.py
@@ -1,52 +1,49 @@
 
-from selenium import webdriver
-
-def select_month(month):
-    elements = driver.find_elements_by_class_name('ui-datepicker-current-day')
-    for element in elements:
-        if element.get_attribute('data-value') == month
-        element.click()
-        return
 
 
+def get_days(month, year):
+    if month == 2:
+        if year in [2000,2004,2008,2012]: #leap years
+            days = 29
+        else:
+            days = 28
+    elif month in [9,4,6,11]:
+        days = 30
+    else:
+        days = 31
+    return days
 
-def select_day():
-
-
-
-def download():
-
-
-//*[@id="calendar0"]/div/table/tbody/tr[2]/td[6]/a
-
-//*[@id="calendar0"]/div/table/tbody/tr[2]/td[4]/a
-
-//*[@id="calendar0"]/div/table/tbody/tr[2]/td[4]
-
-//*[@id="calendar11"]/div/table/tbody/tr[5]/td[5]
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == 'main':
+if __name__ == '__main__':
+    from selenium import webdriver
+    import time
+    import random
+    import urllib
 
     path_to_chromedriver ='/Users/Lin/Downloads/chromedriver'
 
     browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+    geotiff_xpath = '//*[@id="content"]/div/div[2]/div[1]/div/div[2]/div[2]/form/table/tbody/tr[1]/td/select/option[3]'
+    download_url = '//*[@id="content"]/div/div[2]/div[1]/div/div[2]/div[2]/form/table/tbody/tr[5]/td/a'
+    dates = []
+    for year in range(1998,2016):
+        for month in range(1,13):
+            days = get_days(month,year)
+            for day in range(1,days+1):
+                dates.append("{}-{}-{}".format(year,"{0:0=2d}".format(month),"{0:0=2d}".format(day)))
+    #putting the dates in a list means I can resume from a certain date if any problems
 
+    for date in dates[39:]:
+        url = 'http://neo.sci.gsfc.nasa.gov/view.php?datasetId=TRMM_3B43D&date={}'.format(date)
+        browser.get(url)
+        time.sleep(2+random.randint(1,6))
+        try:
+            browser.find_element_by_xpath(geotiff_xpath).click()
+        except:
+            time.sleep(5)
+            browser.find_element_by_xpath(geotiff_xpath).click()
+        download_link = browser.find_element_by_xpath(download_url)
 
-for year in years:
-    for day in all_the_days:
-    url = 'http://neo.sci.gsfc.nasa.gov/view.php?datasetId=TRMM_3B43D&year={}'.format(year)
-
-    browser.get(url)
-        - select the month and day in the DOM
-        - click download
+        urllib.urlretrieve( download_link.get_attribute('href'), "scraped_tiffs/{}.tiff".format(date))
+        # browser.close()
+        time.sleep(1)
 
