8e0477cedcfb3fa1a512d2f5b8377afbddf3ecdd
Lin Taylor <taylorxlin@gmail.com>
2016-01-13 17:06:36 -0500

    Switch lat and long in string

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import psycopg2
+import json
 
 app = Flask(__name__)
 conn = psycopg2.connect("dbname = 'nasa'")
@@ -11,11 +12,14 @@ def hello_world():
 
 @app.route('/start_date=<start_date>&end_date=<end_date>&latitude=<latitude>&longitude=<longitude>')
 def return_json(start_date, end_date, latitude, longitude):
-    cur.execute("""SELECT ST_nearestvalue(rast,ST_geomfromtext('Point(%s %s)',4326)), forecast_date FROM rainfall.rasters \
+    cur.execute("""SELECT forecast_date, ST_nearestvalue(rast,ST_geomfromtext('Point(%s %s)',4326)) FROM rainfall.rasters \
                     WHERE forecast_date >= %s \
-                    AND forecast_date <= %s;""", (float(latitude), float(longitude), start_date, end_date))
+                    AND forecast_date <= %s;""", (float(longitude), float(latitude), start_date, end_date))
     rows = cur.fetchall()
-    return str(row)
+    json_dict = {}
+    for row in rows:
+        json_dict[row[0][:-5]] = row[1]
+    return json.dumps(json_dict, sort_keys=True)
     return 'Start date = {} \n End date = {} \n Location = {}'.format(start_date, end_date, latitude)
 
 @app.errorhandler(404)
