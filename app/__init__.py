0b8380f3389b6a8065f6ac2bb805e69fb2e04955
Lin Taylor <taylorxlin@gmail.com>
2016-01-13 14:19:05 -0500

    App URL feature returns list of data points

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,27 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from geoalchemy2.types import Raster
+import psycopg2
 
 app = Flask(__name__)
-
-# app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/nasa'
-app.config['SQLALCHEMY_ECHO'] = True
-app.debug = True
-
-db = SQLAlchemy(app)
-
-class Nasa(db.Model):
-    __tablename__ = 'rasters'
-    __table_args__ = {'schema' : 'rainfall'}
-    rid = db.Column(db.Integer, primary_key=True)
-    rast = db.Column(Raster, unique=True)
-    forecast_date = db.Column(db.String(20), unique=True)
-
-    def __init__(self):
-        self.rid = rid
-        self.rast = rast
-        self.forecast_date = forecast_date
-
-    def __repr__(self):
-        return '<RID {}>'.format(self.rid)
+conn = psycopg2.connect("dbname = 'nasa'")
+cur = conn.cursor()
 
 @app.route('/')
 def hello_world():
     return 'NASA rainfall data'
 
-@app.route('/start_date=<start_date>&end_date=<end_date>&location=<location>')
-def return_json(start_date, end_date, location):
-    return 'Start date = {} \n End date = {} \n Location = {}'.format(start_date, end_date, location)
+@app.route('/start_date=<start_date>&end_date=<end_date>&latitude=<latitude>&longitude=<longitude>')
+def return_json(start_date, end_date, latitude, longitude):
+    cur.execute("""SELECT ST_nearestvalue(rast,ST_geomfromtext('Point(%s %s)',4326)), forecast_date FROM rainfall.rasters \
+                    WHERE forecast_date >= %s \
+                    AND forecast_date <= %s;""", (float(latitude), float(longitude), start_date, end_date))
+    rows = cur.fetchall()
+    return str(row)
+    return 'Start date = {} \n End date = {} \n Location = {}'.format(start_date, end_date, latitude)
 
 @app.errorhandler(404)
 def not_found(error):
     return "Page not found"
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-
-
-# from app import views
